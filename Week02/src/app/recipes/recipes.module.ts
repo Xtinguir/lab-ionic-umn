210d302852d57e9c174f8fd35cff75948a145d90
Xtinguir <marioalexander998@gmail.com>
2019-08-21 21:59:57 +0700

    menu fiex

--- Week02/src/app/recipes/recipes.module.ts
+++ Week02/src/app/recipes/recipes.module.ts
@@ -8,9 +8,10 @@ import { IonicModule } from '@ionic/angular';
 import { RecipesPage } from './recipes.page';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesPageModule' },
-  { path: 'recipe-detail', loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule'}, 
+  {
+    path: '',
+    component: RecipesPage
+  }
 ];
 
 @NgModule({
