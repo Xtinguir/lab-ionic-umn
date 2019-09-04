73899d215233e8bc00a0805f94dff342ffa633fc
Xtinguir <marioalexander998@gmail.com>
2019-09-04 11:42:38 +0700

    Week 04 discover and offer not finished

--- Week04/src/app/places/discover/discover.page.ts
+++ Week04/src/app/places/discover/discover.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiscoverPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuCtrl: MenuController) { }
 
   ngOnInit() {
   }
 
+  onOpenMenu(){
+    this.menuCtrl.toggle('m1');
+  }
 }
